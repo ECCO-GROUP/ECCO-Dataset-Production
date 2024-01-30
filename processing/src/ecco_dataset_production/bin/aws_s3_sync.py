cee5ddff0c2b8a0d291a6846e314875c5cff4ede
Greg Moore <greg.moore@jpl.nasa.gov>
2024-01-30 14:54:51 -0800

    Use raw strings for re

--- processing/src/ecco_dataset_production/bin/aws_s3_sync.py
+++ processing/src/ecco_dataset_production/bin/aws_s3_sync.py
@@ -59,7 +59,7 @@ def is_s3_uri(path_or_uri_str):
     Returns:
         True if string matches 's3://', False otherwise.
     """
-    if re.search( 's3:\/\/', path_or_uri_str, re.IGNORECASE):
+    if re.search( r's3:\/\/', path_or_uri_str, re.IGNORECASE):
         return True
     else:
         return False
