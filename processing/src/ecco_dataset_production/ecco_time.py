6da441fa911392d889a99aebfa1fb8f6de83d468
Greg Moore <greg.moore@jpl.nasa.gov>
2024-06-07 10:43:54 -0700

    Remove cfg input, make arguments explicit, update unit test case

--- processing/src/ecco_dataset_production/ecco_time.py
+++ processing/src/ecco_dataset_production/ecco_time.py
@@ -3,26 +3,28 @@ import ecco_v4_py
 import numpy as np
 
 def make_time_bounds_metadata( granule_time=None,
-    model_timestep=3600, model_timestep_units='s',
-    averaging_period=None, cfg=None):
+    model_start_time=None, model_end_time=None,
+    model_timestep=None, model_timestep_units=None,
+    averaging_period=None):
+
     """If ECCO model start and end times do not align on 24 hour boundaries,
     adjust first and last time averaging period output endpoints accordingly.
 
     Args:
         granule_time (int or str): ECCO analysis time value per time string in
             file name (e.g., '0000000012' or 12, etc.).
+        model_start_time (str): ECCO model start time, in ISO Date Time format
+            ('YYYY-MM-DDThh:mm:ss')
+        model_end_time (str): ECCO model end time, in ISO Date Time format
+            ('YYYY-MM-DDThh:mm:ss')
         model_timestep (int): Number of analysis time steps per granule_time
             increment (e.g., 3600 for one second time steps and granule_time
-            increments of one hour). (default: 3600)
+            increments of one hour).
         model_timestep_units (str): model_timestep units, per NumPy Datetime64
             conventions, e.g., hours ('h'), minutes ('m'), seconds ('s') etc.
-            (default: 's')
         averaging_period (str): Interval for which adjusted time bounds will be
             created ('AVG_YEAR', 'AVG_MON', 'AVG_WEEK', or 'AVG_DAY' (case
             insensitive)).
-        cfg (dict): ECCO Dataset Production configuration data, typically parsed
-            from yaml input (see examples). Specifically, this routine looks for
-            the keys, 'model_start_time' and 'model_end_time'.
 
     Returns:
         (tb,center_time): Tuple of tb time bounds array (tb[0]=interval start
@@ -40,26 +42,26 @@ def make_time_bounds_metadata( granule_time=None,
     # given a granule_time, get interval start, end, and center times:
 
     tb, center_time = ecco_v4_py.ecco_utils.make_time_bounds_from_ds64(
-        np.datetime64(cfg['model_start_time']) + 
+        np.datetime64(model_start_time) + 
         np.timedelta64(int(granule_time)*model_timestep,model_timestep_units),
         averaging_period.upper())
 
     # ...and if one of the interval's endpoints lands on either the very first
     # or last day, adjust time boundaries accordingly:
 
-    if tb[0].astype('datetime64[D]')==np.datetime64(cfg['model_start_time'],'D'):
+    if tb[0].astype('datetime64[D]')==np.datetime64(model_start_time,'D'):
         # start of averaging period lands in first day's interval:
-        tb[0] = np.datetime64(cfg['model_start_time'])
+        tb[0] = np.datetime64(model_start_time)
         center_time = tb[0] + (tb[1]-tb[0])/2   # delta time math to avoid
                                                 # exceptions
-    elif tb[1].astype('datetime64[D]')==np.datetime64(cfg['model_end_time'],'D'):
+    elif tb[1].astype('datetime64[D]')==np.datetime64(model_end_time,'D'):
         # end of averaging period lands in last day's interval: move lower bound
         # up by an amount equal to the amount of time left in the last day
         # (because the averaging interval might be a day, month, or year), so it
         # aligns on a 24 hour boundary:
         tb[0] = tb[0] + \
             np.timedelta64(1,'D') - \
-            (np.datetime64(cfg['model_end_time'])-np.datetime64(cfg['model_end_time'],'D'))
+            (np.datetime64(model_end_time)-np.datetime64(model_end_time,'D'))
         center_time = tb[0] + (tb[1]-tb[0])/2   # delta time math to avoid
                                                 # exceptions
 
--- processing/tests/test_ecco_time.py
+++ processing/tests/test_ecco_time.py
@@ -7,8 +7,10 @@ import ecco_dataset_production
 
 # some configuration test data:
 cfg = {
-    'model_start_time': '1992-01-01T12:00:00',
-    'model_end_time':   '2017-12-31T12:00:00'
+    'model_start_time'      : '1992-01-01T12:00:00',
+    'model_end_time'        : '2017-12-31T12:00:00',
+    'model_timestep'        : 1,
+    'model_timestep_units'  : 'h',
 }
 
 
@@ -20,8 +22,11 @@ class TestECCOTime(unittest.TestCase):
         tb,center_time = \
             ecco_dataset_production.ecco_time.make_time_bounds_metadata(
                 granule_time='0000000018',
-                model_timestep=1, model_timestep_units='h',
-                averaging_period='AVG_DAY', cfg=cfg)
+                model_start_time=cfg['model_start_time'],
+                model_end_time=cfg['model_end_time'],
+                model_timestep=cfg['model_timestep'],
+                model_timestep_units=cfg['model_timestep_units'],
+                averaging_period='AVG_DAY')
         self.assertListEqual(
             list(tb),
             [np.datetime64('1992-01-01T12:00:00.000000'), np.datetime64('1992-01-02T06:00:00.000000')])
@@ -32,8 +37,11 @@ class TestECCOTime(unittest.TestCase):
         tb,center_time = \
             ecco_dataset_production.ecco_time.make_time_bounds_metadata(
                 granule_time='0000227904',
-                model_timestep=1, model_timestep_units='h',
-                averaging_period='AVG_DAY', cfg=cfg)
+                model_start_time=cfg['model_start_time'],
+                model_end_time=cfg['model_end_time'],
+                model_timestep=cfg['model_timestep'],
+                model_timestep_units=cfg['model_timestep_units'],
+                averaging_period='AVG_DAY')
         self.assertListEqual(
             list(tb),
             [np.datetime64('2017-12-31T00:00:00.000000'), np.datetime64('2017-12-31T12:00:00.000000')])
