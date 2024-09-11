4c78b8c82ebb44825b29e460e78d170224058e16
Greg Moore <greg.moore@jpl.nasa.gov>
2024-09-10 17:07:49 -0700

    Added to_latlon() to ECCOMDSDataset class

--- processing/src/ecco_dataset_production/ecco_dataset.py
+++ processing/src/ecco_dataset_production/ecco_dataset.py
@@ -1,36 +1,44 @@
 """
 """
 
+import logging
 import numpy as np
 import os
+import pandas as pd
 import shutil
 import tempfile
 import xarray as xr
-#from xmitgcm import open_mdsdataset
 
 import ecco_v4_py
 
 from . import aws
-#from . import ecco_aws_s3_cp
-#from . import ecco_aws_s3_sync
 from . import ecco_file
 from . import ecco_grid
+from . import ecco_mapping_factors
 from . import ecco_task
 
 
+log = logging.getLogger('edp.'+__name__)
+
+
 class ECCOMDSDataset(object):
-    """Class that organizes production-oriented operations on ECCO MDS results
-    datasets.
+    """Class that supports dataset production-oriented operations on ECCO
+    results datasets.
 
     Args:
         task (str, dict, or taskobj): (Path and) name of json-formatted file
             defining a single task, single task description dictionary, or
             ECCOTask object.
         variable (str) : Selected task list variable.
-        grid (ECCOGrid object): Optional grid object. If not provided, grid will
-            be determined from task input. Primarily intended to optimize i/o
-            performance by allowing operations on collections to share a single
-            copy of an ECCOGrid object created by a top-level application.
+        grid (ECCOGrid object): Optional grid object. If not provided, grid data
+            will be fetched using input task descriptor. Primarily intended to
+            optimize i/o performance by allowing collections to share a single
+            copy of an ECCOGrid object created by the calling application.
+        mapping_factors (ECCOMappingFactors object): Optional mapping_factors
+            object. If not provided, mapping_factors data will be fetched using
+            input task descriptor.  Primarily intended to optimize i/o
+            performance by allowing collections to share a single copy of an
+            ECCOMappingFactors object created by the calling application.
         cfg (dict): ECCO Dataset Production configuration data. Referenced
             fields include:
             ecco_native_grid_filename: ECCO NetCDF native grid file name (e.g.,
@@ -45,8 +53,11 @@ class ECCOMDSDataset(object):
         data_dir (str): Temporary directory name for local store of input ECCO
             MDS files specified by task and variable.
         grid (ECCOGrid object): Local reference to ecco_grid input, if
-            provided, or local object if not (and thus generated using task
-            object specifier).
+            provided, or local object if not (and thus fetched using task object
+            specifier).
+        mapping_factors (ECCOMappingFactors object): Local reference to
+            ecco_mapping_factors input, if provided, or local object if not (and
+            thus fetched using task object specifier).
         task (ECCOTask): Local object store of input task descriptor.
         tmp_grid_dir (TemporaryDirectory): Temporary local ECCO grid directory
             if non-local source as specified in task descriptor.
@@ -54,13 +65,30 @@ class ECCOMDSDataset(object):
             for task and variable combination.
 
     """
-    def __init__( self, task=None, grid=None, variable=None, cfg=None,
-        **kwargs):
+    def __init__( self, task=None, variable=None, grid=None,
+        mapping_factors=None, cfg=None, **kwargs):
         """Create instance of ECCOMDSDataset class.
 
         """
         self.task = None
+        self.grid = None
+        self.mapping_factors = None
         self.cfg = cfg
+
+        if grid:
+            if isinstance(grid,ecco_grid.ECCOGrid):
+                # point to existing (input) object:
+                self.grid = grid
+            else:
+                raise ValueError('Input grid must be an instance of ECCOGrid class.')
+
+        if mapping_factors:
+            if isinstance(mapping_factors,ecco_mapping_factors.ECCOMappingFactors):
+                # point to existing (input) object:
+                self.mapping_factors = mapping_factors
+            else:
+                raise ValueError(
+                    'Input mapping_factors must be an instance of ECCOMappingFactors class.')
 
         if task:
 
@@ -74,15 +102,16 @@ class ECCOMDSDataset(object):
             # create dataset using variable and data references in task
             # description:
 
-            # ensure grid data exists locally:
-            if not grid:
-                # create grid from task definition:
+            # ensure grid data are locally accessible:
+            if not self.grid:
+                # fetch grid using task definition:
                 self.grid = ecco_grid.ECCOGrid(task=self.task,**kwargs)
-            elif isinstance(grid,ecco_grid.ECCOGrid):
-                # point to existing object:
-                self.grid = grid
-            else:
-                raise ValueError('If provided, grid must be an instance of ECCOGrid class.')
+
+            # ensure mapping factors are locally accessible:
+            if not self.mapping_factors:
+                # fetch mapping factors using task definition:
+                self.mapping_factors = ecco_mapping_factors.ECCOMappingFactors(
+                    task=self.task,**kwargs)
 
             # gather variable input locally (not the most efficient approach for
             # data that may already be stored locally, but cleanest way to
@@ -100,53 +129,161 @@ class ECCOMDSDataset(object):
                         aws.ecco_aws_s3_cp.aws_s3_cp( src=file, dest=self.data_dir, **kwargs)
 
             if self.task.is_variable_single_component(variable):
+
                 # direct ingest:
-                # use first file in the list (.data file) to provide some of the
-                # input required by load_ecco_vars_from_mds:
-                mds_file = ecco_file.ECCOMDSFilestr(
-                    os.path.basename(
-                        self.task.variable_inputs(variable)[0][0]))
 
-                # back-compatibility with ecco_v4_py.read_bin_llc.load_ecco_vars_from_mds:
-                if self.task['dynamic_metadata']['time_coverage_duration'] == 'P1D':
-                    output_freq_code = 'AVG_DAY'
-                elif self.task['dynamic_metadata']['time_coverage_duration'] == 'P1M':
-                    output_freq_code = 'AVG_MON'
-                elif self.task['dynamic_metadata']['time_coverage_duration'] == 'PT0S':
-                    output_freq_code = 'SNAP'
+                # use first file in the list to tell us something about how it's
+                # to be read:
+                _,ext = os.path.splitext(
+                    self.task.variable_inputs(variable)[0][0])
+
+                if ext == '.data' or ext == '.meta':
+
+                    # time-dependent ECCO results from compact data/meta files:
+
+                    mds_file = ecco_file.ECCOMDSFilestr(
+                        os.path.basename(
+                            self.task.variable_inputs(variable)[0][0]))
+
+                    # back-compatibility with ecco_v4_py.read_bin_llc.load_ecco_vars_from_mds:
+                    if self.task['dynamic_metadata']['time_coverage_duration'] == 'P1D':
+                        output_freq_code = 'AVG_DAY'
+                    elif self.task['dynamic_metadata']['time_coverage_duration'] == 'P1M':
+                        output_freq_code = 'AVG_MON'
+                    elif self.task['dynamic_metadata']['time_coverage_duration'] == 'PT0S':
+                        output_freq_code = 'SNAP'
+                    else:
+                        e1 = "Unknown task['dynamic_metadata']['time_coverage_duration'] type:"
+                        e2 = self.task['dynamic_metadata']['time_coverage_duration']
+                        log.error('%s %s',e1,e2)
+                        raise RuntimeError(f'{e1} {e2}')
+
+                    self.ds = ecco_v4_py.read_bin_llc.load_ecco_vars_from_mds(
+                        mds_var_dir             = self.data_dir,
+                        mds_grid_dir            = self.grid.grid_dir,
+                        mds_files               = mds_file.prefix+'_'+mds_file.averaging_period,
+                        vars_to_load            = mds_file.prefix,
+                        #vars_to_load            = variable,
+                        drop_unused_coords      = True,
+                        grid_vars_to_coords     = False,
+                        output_freq_code        = output_freq_code,
+                        model_time_steps_to_load= [mds_file.time],
+                        read_grid               = True,
+                        #read_grid               = self.cfg['read_grid'],
+                        model_start_datetime    = np.datetime64(self.cfg['model_start_time']))
+
+                    if mds_file.prefix != variable:
+                        self.ds = self.ds.rename_vars({mds_file.prefix:variable})
+
+                elif ext == '.nc':
+
+                    # 1D time-invariant results:
+
+                    # TODO:
+                    pass
+
                 else:
-                    e1 = "Unknown task['dynamic_metadata']['time_coverage_duration'] type:"
-                    e2 = self.task['dynamic_metadata']['time_coverage_duration']
-                    raise RuntimeError(f'{e1} {e2}')
-
-                self.ds = ecco_v4_py.read_bin_llc.load_ecco_vars_from_mds(
-                    mds_var_dir             = self.data_dir,
-                    mds_grid_dir            = self.grid.grid_dir,
-                    mds_files               = mds_file.prefix+'_'+mds_file.averaging_period,
-                    vars_to_load            = mds_file.prefix,
-                    #vars_to_load            = variable,
-                    drop_unused_coords      = True,
-                    grid_vars_to_coords     = False,
-                    output_freq_code        = output_freq_code,
-                    model_time_steps_to_load= [mds_file.time],
-                    read_grid               = True,
-                    #read_grid               = self.cfg['read_grid'],
-                    model_start_datetime    = np.datetime64(self.cfg['model_start_time']))
-
-                if mds_file.prefix != variable:
-                    self.ds = self.ds.rename_vars({mds_file.prefix:variable})
-
-#                previously:
-#                self.ds = open_mdsdataset(
-#                    data_dir=self.data_dir, grid_dir=self.grid.grid_dir,
-#                    read_grid=self.cfg['read_grid'],
-#                    geometry=self.cfg['model_geometry'],
-#                    prefix=mds_file.prefix+'_'+mds_file.averaging_period,
-#                    iters=[mds_file.time])
+                    err = f"Unrecognized file type (extension = '{ext}')"
+                    log.error(err)
+                    raise RuntimeError(err)
 
             else:
                 # vector summation required before ingest:
                 pass    # for now...
+
+
+    def as_latlon( self, variable=None):
+        """Recast variable in latlon format, return as a named (using variable
+        string) xarray DataArray.
+
+        """
+        if self.task.is_2d:
+
+            # output allocation:
+            variable_as_latlon = np.zeros(
+                (self.grid.latlon_grid['latitude'].shape[0],
+                self.grid.latlon_grid['longitude'].shape[0]))
+
+            # operate on surface (z=0) only:
+            var = self.ds[variable].data.squeeze()                  # dask array, native grid,
+                                                                    # no singleton dimensions
+            var = var.vindex[self.grid.native_wet_point_indices[0]] # dask array, native grid,
+                                                                    # no singleton dimensions,
+                                                                    # surface wet points only,
+                                                                    # as vector
+            var_latlon = self.mapping_factors.native_to_latlon_mapping_factors(level=0).T.dot(var)
+                                                                    # numpy (vector) array,
+                                                                    # as latlon
+            var_latlon_land_masked = np.where(
+                np.isnan(self.mapping_factors.latlon_land_mask(level=0)), np.nan, var_latlon)
+                                                                    # land values as NaNs
+            variable_as_latlon[:] = var_latlon_land_masked.reshape( # numpy 1D array to
+                self.grid.latlon_grid['latitude'].shape[0],         # lat x lon array
+                self.grid.latlon_grid['longitude'].shape[0])
+
+            # add a "time" axis ((lat,lon) -> (time,lat,lon)):
+            variable_as_latlon = np.expand_dims(variable_as_latlon,0)
+
+            variable_as_latlon_da = xr.DataArray(
+                name=variable,
+                data=variable_as_latlon,
+                dims=['time','latitude','longitude'],
+                coords=[
+                    [pd.Timestamp(self.task['dynamic_metadata']['time_coverage_center'])],
+                    self.grid.latlon_grid['latitude'].data,
+                    self.grid.latlon_grid['longitude'].data])
+
+        elif self.task.is_3d:
+
+            # num vertical depths:
+            nz = self.grid.latlon_grid.sizes['Z']
+
+            # output allocation:
+            variable_as_latlon = np.zeros(
+                (nz,
+                self.grid.latlon_grid['latitude'].shape[0],
+                self.grid.latlon_grid['longitude'].shape[0]))
+
+            var = self.ds[variable].data.squeeze()                      # dask array, native grid,
+                                                                        # no singleton dimensions
+            for z in range(nz):
+
+                var_z = var[z,:].vindex[                                # dask array, native grid,
+                    self.grid.native_wet_point_indices[z]]              # no singleton dimensions,
+                                                                        # level z wet points only,
+                                                                        # as vector
+                var_z_latlon = \
+                    self.mapping_factors.native_to_latlon_mapping_factors(level=z).T.dot(var_z)
+                                                                        # numpy (vector) array,
+                                                                        # level z, as latlon
+                var_z_latlon_land_masked = np.where(
+                    np.isnan(self.mapping_factors.latlon_land_mask(level=z)), np.nan, var_z_latlon)
+                                                                        # land values, level z,
+                                                                        # as NaNs
+                variable_as_latlon[z,:] = var_z_latlon_land_masked.reshape(
+                    self.grid.latlon_grid['latitude'].shape[0],         # numpy 1D array to
+                    self.grid.latlon_grid['longitude'].shape[0])        # lat x lon array
+
+
+            # add a "time" axis ((z,lat,lon) -> (time,z,lat,lon)):
+            variable_as_latlon = np.expand_dims(variable_as_latlon,0)
+
+            variable_as_latlon_da = xr.DataArray(
+                name=variable,
+                data=variable_as_latlon,
+                dims=['time','Z','latitude','longitude'],
+                coords=[
+                    [pd.Timestamp(self.task['dynamic_metadata']['time_coverage_center'])],
+                    self.grid.latlon_grid['Z'].data,
+                    self.grid.latlon_grid['latitude'].data,
+                    self.grid.latlon_grid['longitude'].data])
+
+        # note: could "promote" to xr.Dataset and add time_bnds coordinates here
+        # as had been done in the original code but, since this is done during
+        # dataset production by the calling, and folow-on metadata/attributes,
+        # code just skip for now.
+
+        return variable_as_latlon_da
 
 
     def apply_land_mask_to_native_variable( self, variable=None):
@@ -233,7 +370,7 @@ class ECCOMDSDataset(object):
 
 
     def is_variable_3d( self, variable=None):
-        """Determines wheter or not specified variable is three-dimensional by
+        """Determines whether or not specified variable is three-dimensional by
         checking to see if any of its dimensions are indexed by (a) string(s)
         that include(s) 'k'.
 
@@ -248,7 +385,7 @@ class ECCOMDSDataset(object):
 
 
     def drop_all_variables_except( self, variable=None):
-        """Ensures that no other than specified variable exist in the dataset.
+        """Ensures that no other than specified variable exists in the dataset.
 
         Args:
             variable (str): ECCO resuls variable name.
